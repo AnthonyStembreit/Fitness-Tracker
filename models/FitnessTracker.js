4e23ef53dcfd2895a669c7a64ec1117cc2de0f57
AnthonyStembreit <stembreit@gmail.com>
2020-10-07 16:36:20 -0500

    created exercises seperate from date in order for the seeds to work properly

--- models/FitnessTracker.js
+++ models/FitnessTracker.js
@@ -1,38 +1,41 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const Excercises = new Schema ({
+const Workout = new Schema({
     date: {
         type: Date,
         default: Date.now
     },
-    type:{
-        type: String,
-        trim: true,
-        required: "String is Required"
-    },
-    name:{
-        type: String,
-        trim: true,
-        required: "String is Required"
-    },
-    duration:{
-        type: Number,
-        required: true
-    },
-    weight:{
-        type: Number,
-    },
-    reps:{
-        type: Number,
-    },
-    sets:{
-        type: Number,
-    },
-    distance:{
-        type: Number,
-    }
+    exercises:
+        [{
+            type: {
+                type: String,
+                trim: true,
+                required: "Choice is required"
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Name is Required"
+            },
+            duration: {
+                type: Number,
+                required:"Duration is Required"
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            distance: {
+                type: Number
+            }
+        }]
 })
-const FitTracker = mongoose.model("Excercises", Excercises);
+const FitTracker = mongoose.model("FitTracker", Workout);
 
 module.exports = FitTracker;
